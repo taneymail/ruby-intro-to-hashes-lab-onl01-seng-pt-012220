acc82ddf35d736d6f9730f50ea5bb71cac23be22
T S <taneymail@gmail.com>
2020-02-02 20:17:50 +0000

    Done.

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -14,8 +14,8 @@ describe "working with hashes" do
 
   describe "instantiating a hash with ONE key-value pair" do
     describe "#actor" do
-      it "creates a hash named actor whose key is a symbol :name and whose value is a string, 'Dwayne The Rock Johnson'" do
-        expect(actor[:name]).to eq("Dwayne The Rock Johnson")
+      it "creates a hash named actor whose key is a symbol :name and whose value is a string, 'Tom Hanks'" do
+        expect(actor[:name]).to eq("Tom Hanks")
       end
     end
   end
